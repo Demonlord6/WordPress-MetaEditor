9af60bacec86d5b26675e31e1d9b22bdee9ef35c
Demonlord6 <antoniopedrosilva6@gmail.com>
2017-08-25 22:31:18 +0100

    Update wordpress-meta-editor.php

--- wordpress-meta-editor.php
+++ wordpress-meta-editor.php
@@ -2,7 +2,7 @@
 /*
 	Plugin Name: WordPress - MetaEditor
 	Plugin URI:  https://github.com/Demonlord6/WordPress-MetaEditor
-	Description: Basic WordPress Plugin Header Comment
+	Description: Basic Meta Editor for Wordpress
 	Version:     0.1
 	Author:      Demonlord6
 	Author URI:  https://github.com/Demonlord6
@@ -11,19 +11,19 @@
 */
 	defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
-	add_action( 'admin_menu', 'my_plugin_menu' );
+	/*NEW*/
+
+	add_action( 'admin_menu', 'my_plugin_menu' ); 
 
 	function my_plugin_menu() {
-	add_options_page( 'Meta Editor', 'Meta Editor', 'meta-editor', 'my-unique-identifier', 'my_plugin_options' );
-	}
 
+	add_options_page( 'WP Meta Editor', 'WP Meta Editor', 'manage_options', 'wp-meta-editor', 'my_plugin_options' );
 
-	function my_plugin_options() {
-		if ( !current_user_can( 'manage_options' ) )  {
-			wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-		}
-		echo '<div class="wrap">';
-		echo '<p>Here is where the form would go if I actually had options.</p>';
-		echo '</div>';
-	}
+} 
+function my_plugin_options() { 
+if ( !current_user_can( 'manage_options' ) ) { 
+ wp_die( __( 'You do not have sufficient permissions to access this page.' ) ); 
+} 
+echo '<div class="wrap"><h2>WP Meta Editor</h2></div>'; 
+}
 ?>
